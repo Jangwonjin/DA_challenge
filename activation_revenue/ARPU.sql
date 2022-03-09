18c938211a8ddf267a8b2271d4ce00c209997f0a
onejinee <wonjin1008@naver.com>
2022-03-09 16:19:33 +0900

    Update ARPU.sql

--- activation_revenue/ARPU.sql
+++ activation_revenue/ARPU.sql
@@ -1,3 +1,4 @@
+-- ARPU by Daily Active User
 SELECT a.date, sum(r.revenue)/COUNT(DISTINCT a.ID) as ARPU
 FROM data.activation a LEFT OUTER JOIN data.revenue r 
 ON (a.ID = r.ID and a.date = r.date)
